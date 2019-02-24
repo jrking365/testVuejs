808b93251c36447dbec6e4941a9feb2ee6bcdf8c
jrking365 <nigoumijeanroger@gmail.com>
2019-02-24 23:37:03 +0100

    work on class binding

--- vueMastery/main.js
+++ vueMastery/main.js
@@ -2,6 +2,29 @@ var app = new Vue({
     el:"#app",
     data:{
         product:'socks',
-        image:'./assets/vmSocks-green-onWhite.jpg'
-    }
+        image:'./assets/vmSocks-green-onWhite.jpg',
+        inStock:true,
+        details:["80% coton", "20% polyester", "Gender-neutral"],
+        variants:[
+            {
+                variantId:2234,
+                variantColor:"green",
+                variantImage:'./assets/vmSocks-green-onWhite.jpg',
+            },
+            {
+                variantId:2235,
+                variantColor:"blue",
+                variantImage:'./assets/vmSocks-blue-onWhite.jpg'
+            }
+        ],
+        cart:0,
+    },
+    methods: {
+        addToCart: function(){
+            this.cart +=1;
+        },
+        updateProduct(varientImage){
+           this.image = varientImage;
+        }
+    },
 })
